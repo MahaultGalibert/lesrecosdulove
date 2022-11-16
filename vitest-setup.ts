f3bf697126fc8b5ffb0b3e5348af60aa6670653d
Louise Loisel <louiselo@theodo.fr>
2022-11-16 15:06:28 +0100

    Fix rendering tests

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,19 +1,20 @@
-import { vi, expect, afterEach }  from  "vitest";
- import { cleanup }  from  "@testing-library/react";
- import matchers, {
-   TestingLibraryMatchers,
-}  from  "@testing-library/jest-dom/matchers";
+import { vi, expect, afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import matchers, {
+  TestingLibraryMatchers,
+} from "@testing-library/jest-dom/matchers";
 
- declare  global {
-   namespace  Vi {
-     interface  JestAssertion<T =  any>
-       extends jest. Matchers< void, T>,
-         TestingLibraryMatchers<T,  void> {}
+declare global {
+  namespace Vi {
+    interface JestAssertion<T = any>
+      extends jest.Matchers<void, T>,
+        TestingLibraryMatchers<T, void> {}
   }
 }
- // Extend expect from testing-library
+
+// Extend except from testing-library
 expect.extend(matchers);
- // Set the cleanup function globally to avoid manual cleanup of each test file
- afterEach( () => {
-   cleanup();
+// Set the cleanup function globally to avoid manual cleanup of each test file
+afterEach(() => {
+  cleanup();
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,19 @@
-import {defineConfig} from "vitest/config"
-import react from "@vitejs/plugin-react"
-import path from "path"
+/// <reference types="vitest" />
 
-export  default  defineConfig({
-    plugins: [ react()],
-    test: {
-      environment:  "jsdom",
-      setupFiles:  "./vitest-setup.ts",
-   },
-    resolve: {
-      alias: {
-        "@": path. resolve(__dirname,  "."),
-     },
-   },
- });
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest-setup.ts",
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
+  },
+});
