941a4dde6cce0d600a81387e50772c9ccd222a56
MahaultGalibert <mahault.destaintot@gmail.com>
2022-11-16 13:15:53 +0100

    mise en place CI et désactivation test créant des pb

---
+++ __tests__/app.test-off.tsx
@@ -0,0 +1,15 @@
+// import React from "react"
+// import { describe, expect, it } from "vitest"
+// import { render, screen } from "@testing-library/react"
+// import "@testing-library/jest-dom"
+
+// function App() {
+//   return <div> app </div>
+// }
+
+// describe("App", () => {
+//   it("it should be rendered", () => {
+//     render(<App />)
+//     expect(screen.getByText("app")).toBeInTheDocument()
+//   })
+// })
