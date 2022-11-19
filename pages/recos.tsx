5e1857b3b9e091489cd2a99cd189d09ecad504ff
MahaultGalibert <mahault.destaintot@gmail.com>
2022-11-19 22:06:47 +0100

    formattage recos

--- pages/recos.tsx
+++ pages/recos.tsx
@@ -1,17 +1,17 @@
 // pages/recos.tsx
 
-import React from 'react';
-import { GetServerSideProps } from 'next';
-import { useSession, getSession } from 'next-auth/react';
-import Layout from '../components/Layout';
-import Post, { PostProps } from '../components/Post';
-import prisma from '../lib/prisma';
+import React from "react"
+import { GetServerSideProps } from "next"
+import { useSession, getSession } from "next-auth/react"
+import Layout from "../components/Layout"
+import Post, { PostProps } from "../components/Post"
+import prisma from "../lib/prisma"
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const session = await getSession({ req });
+  const session = await getSession({ req })
   if (!session) {
-    res.statusCode = 403;
-    return { props: { recos: [] } };
+    res.statusCode = 403
+    return { props: { recos: [] } }
   }
 
   const recos = await prisma.post.findMany({
@@ -23,26 +23,26 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
         select: { name: true },
       },
     },
-  });
+  })
   return {
     props: { recos },
-  };
-};
+  }
+}
 
 type Props = {
-  recos: PostProps[];
-};
+  recos: PostProps[]
+}
 
 const Recos: React.FC<Props> = (props) => {
-  const { data: session } = useSession();
+  const { data: session } = useSession()
 
   if (!session) {
     return (
       <Layout>
         <h1 className="ml-4 mt-2 text-3xl font-bold">Mes recos</h1>
-        <div>Vous devez être connecté pour voir cette page.</div>
+        <div className="text-sm ml-4 mt-8">Vous devez être connecté pour voir cette page.</div>
       </Layout>
-    );
+    )
   }
 
   return (
@@ -58,25 +58,24 @@ const Recos: React.FC<Props> = (props) => {
         </main>
       </div>
       <style jsx>{`
-        
         .post-box-div {
           background: white;
           margin: 1rem;
           padding: 0;
           border-style: solid;
           border-width: 0.01rem;
-          border-color: rgb(220,220,220);
+          border-color: rgb(220, 220, 220);
           border-radius: 0.3rem;
           height: 10rem;
           width: 15rem;
         }
 
         #feed-box-div {
-            display: flex;
-            flex-wrap: wrap;
-            margin: 3rem 10rem;
-            justify-content: space-evenly;
-          }
+          display: flex;
+          flex-wrap: wrap;
+          margin: 3rem 10rem;
+          justify-content: space-evenly;
+        }
 
         .post:hover {
           box-shadow: 1px 1px 3px #aaa;
@@ -87,7 +86,7 @@ const Recos: React.FC<Props> = (props) => {
         }
       `}</style>
     </Layout>
-  );
-};
+  )
+}
 
-export default Recos;
+export default Recos
